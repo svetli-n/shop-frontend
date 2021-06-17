7c1c44184e253cf47cdf732e39bf76ed296a5a64
Svetlin Stefanov <s.m.stefanov@gmail.com>
2021-06-18 00:54:27 +0200

    Search as you type and display.

--- src/pages/item_list.rs
+++ src/pages/item_list.rs
@@ -1,18 +1,74 @@
 use crate::pages::item::Item;
-use yew::{html, Component, ComponentLink, Html, Properties};
+use yew::{html, Component, ComponentLink, Html, Properties, InputData};
+use yew::{events::KeyboardEvent, Classes};
+use std::collections::HashMap;
+use yew::format::{Json, Nothing};
+use anyhow::Error;
+use yew::services::fetch::{FetchService, FetchTask, Request, Response};
+use crate::model;
 
-pub struct ItemList {}
+pub enum Msg {
+    Search(String),
+    FetchReady(Result<Vec<HashMap<String, String>>, Error>),
+}
+
+
+pub struct ItemList {
+    list: Option<Vec<HashMap<String, String>>>,
+    link: ComponentLink<Self>,
+    _ft: Option<FetchTask>,
+}
+
+impl ItemList {
+
+    fn fetch_json(&mut self, query: String) -> FetchTask {
+        let callback = self.link.batch_callback(
+            move |response: Response<Json<Result<Vec<HashMap<String, String>>, Error>>>| {
+                let (meta, Json(data)) = response.into_parts();
+                log::info!("META: {:?}, {:?}", meta, data);
+                if meta.status.is_success() {
+                    Some(Msg::FetchReady(data))
+                } else {
+                    log::info!("{:?}", meta);
+                    None // FIXME: Handle this error accordingly.
+                }
+            },
+        );
+
+        let url = format!("http://localhost:5000/search/{}", query);
+        let request = Request::get(url).body(Nothing).unwrap();
+        FetchService::fetch(request, callback).unwrap()
+    }
+
+}
 
 impl Component for ItemList {
-    type Message = ();
+    type Message = Msg;
     type Properties = ();
 
     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        ItemList {}
+        log::info!("ItemList");
+        ItemList {
+            list: None,
+            link,
+            _ft: None,
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> bool {
-        todo!()
+        match msg {
+            Msg::Search(value) => {
+                log::info!("msg: {}", value);
+                let task = self.fetch_json(value);
+                self._ft = Some(task);
+            },
+            Msg::FetchReady(response) => {
+                log::info!("FetchReady");
+                self.list = response.ok();
+                log::info!("Got: {:?}", self.list);
+            },
+        }
+        true
     }
 
     fn change(&mut self, _props: Self::Properties) -> bool {
@@ -20,27 +76,27 @@ impl Component for ItemList {
     }
 
     fn view(&self) -> Html {
-        let items = (1..3).map(|i| {
-            html! {
-                <div class="tile is-ancestor">
-                    <div class="tile is-2">
-                        <p></p>
-                    </div>
-                    <div class="tile is-3 box is-warning has-background-grey-light">
-                        <Item id=i />
-                    </div>
-                    <button class="button is-dark is-large ml-1">{"Buy"}</button>
-                    <div class="tile">
-                        <p></p>
-                    </div>
-                </div>
-            }
-        });
-        html! {
-                <div class="tile is-ancestor is-vertical mt-6 ml-3">
-                    {for items}
-                </div>
 
+        html! {
+            <div class="tile is-vertical mt-6 ml-3">
+                <input
+                    class="tile is-3 box"
+                    placeholder="Search chemicals"
+                    // value=&self.state.value
+                    oninput=self.link.callback(|e: InputData| Msg::Search(e.value))
+                    // onkeypress=self.link.batch_callback(|e: KeyboardEvent| {
+                    //     if e.key() == "Enter" { Some(Msg::Search(e.value)) } else { None }
+                    // })
+                />
+                <div class="tile is-ancestor is-vertical mt-6 ml-3">
+                    {
+                        for self.list.iter().map(|d| {
+                            format!("{:?}", d)
+                        })
+                    }
+                </div>
+            </div>
         }
+
     }
 }
